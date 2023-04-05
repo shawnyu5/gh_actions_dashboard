4cc2aca20696a0b627038efc139c098e41472dac
shawn yu <shawn.yu5@hotmail.com>
2023-04-04 23:10:48 -0400

    fix: make github api request with auth token properly

--- backend/src/github/workflow.rs
+++ backend/src/github/workflow.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use octocrab::{models::Repository, Octocrab, Page};
+use reqwest::header::AUTHORIZATION;
 
 use super::auth::app_auth_token;
 
@@ -13,24 +14,23 @@ pub async fn get_all_org_repos(org_name: &str) -> Result<Page<Repository>> {
 /// get all repos for a user
 ///
 /// * `user_name`: the user to get the repos of
-pub async fn get_all_user_repos(user_name: &str) -> Result<Page<Repository>> {
+pub async fn get_all_user_repos(user_name: &str) -> Result<Vec<Repository>> {
     // https://api.github.com/users/USERNAME/repos
     let query = vec![("type", "owner")];
     let client = reqwest::Client::builder().user_agent(user_name).build()?;
+    println!("{:?}", app_auth_token().await.unwrap());
 
     let res = client
         .get(format!("https://api.github.com/users/{}/repos", user_name))
+        .header(
+            AUTHORIZATION,
+            format!("Bearer {}", app_auth_token().await.unwrap()),
+        )
         .query(&query)
         .send()
-        .await?
-        .json::<Vec<Repository>>()
         .await?;
 
-    let repo_len = res.len();
-    let mut repo_pages: Page<Repository> = Page::default();
-    repo_pages.items = res;
-    repo_pages.incomplete_results = Some(false);
-    repo_pages.total_count = Some(repo_len as u64);
-
-    return Ok(repo_pages);
+    println!("{:?}", &res.headers());
+    let json = res.json::<Vec<Repository>>().await?;
+    return Ok(json);
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,35 @@
+#[macro_use]
+extern crate rocket;
+mod routes;
+use crate::routes::all_user_repos::user_repo;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::{Request, Response};
 mod github;
-// use crate::github::auth::app_auth_token;
 
-#[tokio::main]
-async fn main() {
-    let repos = github::workflow::get_all_user_repos("shawnyu5")
-        .await
-        .unwrap();
+pub struct CORS;
 
-    repos.into_iter().for_each(|repo| println!("{}", repo.name));
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().attach(CORS).mount("/", routes![user_repo])
 }
