76f83ef635d49b8e0ca5a723e5f29f617397f990
shawn yu <shawn.yu5@hotmail.com>
2023-04-11 23:46:02 -0400

    fix: error handling for repo route

--- frontend/src/components/counter/counter.rs
+++ frontend/src/components/counter/counter.rs
@@ -1,7 +1,5 @@
-use std::ops::Deref;
 use gloo_timers::callback::Timeout;
-use log::debug;
-use wasm_bindgen::JsValue;
+use std::ops::Deref;
 use yew::prelude::*;
 use yew::Properties;
 
@@ -11,7 +9,7 @@ use yew::Properties;
 /// * `increment`: the seconds between each increment
 pub struct Props {
     pub increment: u32,
-    pub title: String
+    pub title: String,
 }
 #[function_component(Counter)]
 pub fn counter(props: &Props) -> Html {
@@ -19,18 +17,19 @@ pub fn counter(props: &Props) -> Html {
     {
         let props = props.clone();
         let counter = counter.clone();
-        use_effect_with_deps(move |counter| {
-            let counter = counter.clone();
-            let timeout = Timeout::new(props.increment * 1000, move || {
-                counter.set(*counter.deref() + 1);
-            });
-            timeout.forget();
-        }, counter);
+        use_effect_with_deps(
+            move |counter| {
+                let counter = counter.clone();
+                let timeout = Timeout::new(props.increment * 1000, move || {
+                    counter.set(*counter.deref() + 1);
+                });
+                timeout.forget();
+            },
+            counter,
+        );
     }
 
     html! {
         <span>{&props.title} {*counter}{"s"}</span>
     }
 }
-
-
--- frontend/src/components/repos.rs
+++ frontend/src/components/repos.rs
@@ -5,37 +5,58 @@ use wasm_bindgen::JsValue;
 use yew::prelude::*;
 use yew_hooks::prelude::*;
 
+/// get the user repo's parsed into text
+async fn get_user_repos_text() -> Result<String, reqwest_wasm::Error> {
+    return Ok(
+        reqwest_wasm::get(format!("{}/user/repos", &ENVIRONMENT.api_address))
+            .await?
+            .text()
+            .await?,
+    );
+}
 #[function_component(Repos)]
 pub fn all_repos() -> Html {
-    let repos: UseAsyncHandle<Vec<Repository>, String> = use_async(async move {
-        let response = reqwest_wasm::get(format!("{}/user/repos", &ENVIRONMENT.api_address))
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
+    let loading_state = use_state(|| false);
 
-        let js_obj = JsValue::from(&response);
-        info!("js_obj: {:?}", js_obj);
+    let repos: UseAsyncHandle<Vec<Repository>, String> = {
+        let loading_state = loading_state.clone();
+        use_async(async move {
+            loading_state.set(true);
+            let response = match get_user_repos_text().await {
+                Ok(repos) => repos,
+                Err(e) => {
+                    loading_state.set(false);
+                    return Err(e.to_string());
+                }
+            };
+            let js_obj = JsValue::from(&response);
+            info!("{}", js_obj.as_string().unwrap());
 
-        return match serde_json::from_str(&response) {
-            Ok(repos) => Ok(repos),
-            Err(e) => Err(e.to_string()),
-        };
-    });
+            loading_state.set(false);
+            return match serde_json::from_str(&response) {
+                Ok(repos) => Ok(repos),
+                Err(e) => Err(e.to_string()),
+            };
+        })
+    };
 
     {
         let repos = repos.clone();
         use_effect_with_deps(
             move |_| {
                 repos.run();
-                || ()
             },
             (),
         )
     }
 
-    if let Some(repos) = &repos.data {
+    if *loading_state {
+        html! {
+            <div>
+                <p>{ "Loading..." }</p>
+            </div>
+        }
+    } else if let Some(repos) = &repos.data {
         if repos.is_empty() {
             html! {
                 <div>
@@ -58,7 +79,7 @@ pub fn all_repos() -> Html {
     } else {
         html! {
             <div>
-                <p>{ "Loading..." }</p>
+                <p>{ "Error getting repos" }</p>
             </div>
         }
     }
