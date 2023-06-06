bb3b1cde56e1ffdfe9c3a16628eff7094200a2ef
shawn yu <shawn.yu5@hotmail.com>
2023-06-05 22:32:54 -0400

    feat: leptos 2

---
+++ leptos-2/src/app.rs
@@ -0,0 +1,59 @@
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context(cx);
+
+    view! {
+        cx,
+
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/start-axum.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes>
+                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage(cx: Scope) -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(cx, 0);
+    let on_click = move |_| {
+        create_resource(
+            cx,
+            || (),
+            |_| async move {
+                hello().await.ok();
+            },
+        );
+        set_count.update(|count| *count += 1)
+    };
+
+    view! { cx,
+        <h1>"Welcome to Leptos!"</h1>
+        <button on:click=on_click>"Click Me: " {count}</button>
+    }
+}
+
+#[server(Home, "/api")]
+async fn hello() -> Result<(), ServerFnError> {
+    use reqwest::Client;
+    let _ = Client::new().get("https://google.com").send().await;
+    println!("Hello from the server!");
+    return Ok(());
+}
